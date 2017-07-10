c90b7c1a8bcfc603aad5c35b0dc7628370fe140b
mthorry <mthorry@gmail.com>
2017-07-10 18:26:16 -0400

    Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(names)
+  names.map do |name|
+    name.capitalize.insert(-1, '!')
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |food| food == "cheddar" || food == "gouda" || food == "camembert"}
 end
+
+#^I'm pretty sure there's a loop within a loop situation I could do to make this less hard-coded but I can't figure it out!! D:
+
+
+
+
+
